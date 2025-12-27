47927441360024f873b3c066f3fd2ae03432b613
Chananet <chananet7@gmail.com>
2025-12-27 15:43:49 +1100

    add link and image extraction function

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 
 
@@ -33,3 +34,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return newNode
               
 
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
