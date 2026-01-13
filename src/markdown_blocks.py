f90f1dd4e9148c35471b0988b0e2c62c6869bfae
Chananet <chananet7@gmail.com>
2026-01-13 16:40:07 +1100

    block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     result = []
     splitBlock = markdown.split("\n\n")
@@ -8,3 +20,62 @@ def markdown_to_blocks(markdown):
             result.append(cleanSplit)
 
     return result
+
+
+def block_to_block_type(textBlock):
+    blockSplit = textBlock.split("\n")
+
+    if textBlock.startswith("#"):
+        count = 0
+        while count <= len(textBlock) and textBlock[count] == "#":
+            count += 1
+        
+        if count > 0 and count < 6 and textBlock[count] == " ":
+            return BlockType.HEADING
+        
+
+    if blockSplit[0].startswith('```') and len(blockSplit) > 1:
+        if blockSplit[-1].startswith('```'):
+            return BlockType.CODE
+        
+    if textBlock.startswith("> "):
+        codeA = True
+        for line in blockSplit:
+            if not line.strip().startswith("> "):
+                codeA = False
+                break
+
+        if codeA == True:
+            return BlockType.QUOTE
+
+        
+    if textBlock.startswith("- "):
+        list = True
+        for line in blockSplit:
+            if not line.strip().startswith("- "):
+                list = False
+                break
+
+        if list == True:
+            return BlockType.UNORDERED_LIST
+        
+    
+    if textBlock.startswith("1. "):
+       num = 1
+       order = True
+
+       for line in blockSplit:
+           if line.strip().startswith(f"{num}. "):
+               num += 1
+           else:
+               order = False
+               break
+       if order:
+           return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH   
+       
+
+
+
+           
