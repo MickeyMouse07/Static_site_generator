cf192781804143823a880c870c4da322c83b1569
Chananet <chananet7@gmail.com>
2026-02-07 16:06:49 +1100

    generate page

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
-#from textnode import TextNode, TextType
 import os
 import shutil
 import logging
 from copystatic import copyStatic
+from generatePage import generate_page
 
 def main():
+    
+    # file path for copystatic
     base_dir = os.path.dirname(os.path.dirname(__file__))
     public_path = os.path.join(base_dir, "public")
     static_path = os.path.join(base_dir, "static")
+
+    #file path for generatePage
+    from_path = os.path.join(base_dir,"content","index.md")
+    template_path = os.path.join(base_dir, "template.html")
+    dest_path = os.path.join(base_dir, "public","index.html" )
 
     if os.path.exists(public_path):
         logging.info("Delete public folder successfully!")
@@ -15,6 +22,7 @@ def main():
 
     os.makedirs(public_path)    
     copyStatic(static_path, public_path)
+    generate_page(from_path, template_path, dest_path)
 
 
 main()
